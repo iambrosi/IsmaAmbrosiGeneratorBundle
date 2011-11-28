f3b1572085c759d20887f4efc54fa9eb37e734cf
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-11-28 12:56:42 -0200

    GeneratorBundle

---
+++ Generator/Generator.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace IsmaAmbrosi\GeneratorBundle\Generator;
+
+use \Sensio\Bundle\GeneratorBundle\Generator\Generator as BaseGenerator;
+
+class Generator extends BaseGenerator
+{
+
+}
