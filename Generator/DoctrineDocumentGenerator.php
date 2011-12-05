0e9bae6d7c47925d7755bddc5ce7247986c9272e
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-12-05 14:54:56 -0200

    DocumentGenerator

---
+++ Generator/DoctrineDocumentGenerator.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace IsmaAmbrosi\GeneratorBundle\Generator;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadataInfo;
+use Doctrine\ODM\MongoDB\Tools\DocumentGenerator;
+use Doctrine\ODM\MongoDB\Tools\DocumentRepositoryGenerator;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Util\Filesystem;
+
+use Doctrine\ORM\Tools\Export\ClassMetadataExporter;
+
+/**
+ * Class DoctrineDocumentGenerator
+ *
+ * @author Ismael Ambrosi<ismaambrosi@gmail.com>
+ */
+class DoctrineDocumentGenerator extends Generator
+{
+
+    /**
+     * @var \Symfony\Component\HttpKernel\Util\Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @var \Doctrine\ODM\MongoDB\DocumentManager
+     */
+    private $documentManager;
+
+    /**
+     * Constructor
+     *
+     * @param \Symfony\Component\HttpKernel\Util\Filesystem $filesystem
+     * @param \Doctrine\ODM\MongoDB\DocumentManager         $documentManager
+     */
+    public function __construct(Filesystem $filesystem, DocumentManager $documentManager)
+    {
+        $this->filesystem = $filesystem;
+        $this->documentManager = $documentManager;
+    }
+
+    public function generate(BundleInterface $bundle, $document, array $fields, $withRepository)
+    {
+        $config = $this->documentManager->getConfiguration();
+        $config->addDocumentNamespace($bundle->getName(), $bundle->getNamespace() . '\\Document');
+
+        $documentClass = $config->getDocumentNamespace($bundle->getName()) . '\\' . $document;
+        $documentPath = $bundle->getPath() . '/Document/' . str_replace('\\', '/', $document) . '.php';
+        if (file_exists($documentPath)) {
+            throw new \RuntimeException(sprintf('Document "%s" already exists.', $documentClass));
+        }
+
+        $class = new ClassMetadataInfo($documentClass);
+        if ($withRepository) {
+            $class->setCustomRepositoryClass($documentClass . 'Repository');
+        }
+
+        $class->mapField(array('fieldName' => 'id',
+                               'type'      => 'integer',
+                               'id'        => true));
+        $class->setIdGeneratorType(ClassMetadataInfo::GENERATOR_TYPE_AUTO);
+        foreach ($fields as $field) {
+            $class->mapField($field);
+        }
+
+        $documentGenerator = $this->getDocumentGenerator();
+        $documentCode = $documentGenerator->generateDocumentClass($class);
+
+        $this->filesystem->mkdir(dirname($documentPath));
+        file_put_contents($documentPath, $documentCode, LOCK_EX);
+
+        if ($withRepository) {
+            $path = $bundle->getPath() . str_repeat('/..', substr_count(get_class($bundle), '\\'));
+            $this->getRepositoryGenerator()->writeDocumentRepositoryClass($class->customRepositoryClassName, $path);
+        }
+    }
+
+    protected function getDocumentGenerator()
+    {
+        $documentGenerator = new DocumentGenerator();
+        $documentGenerator->setGenerateAnnotations(true);
+        $documentGenerator->setGenerateStubMethods(true);
+        $documentGenerator->setRegenerateDocumentIfExists(false);
+        $documentGenerator->setUpdateDocumentIfExists(true);
+        $documentGenerator->setNumSpaces(4);
+
+        return $documentGenerator;
+    }
+
+    protected function getRepositoryGenerator()
+    {
+        return new DocumentRepositoryGenerator();
+    }
+}
