3af0d1fb40e10b244a24b2663de2a11b5e8bb0d1
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-12-05 14:57:41 -0200

    DoctrineCommand for MongoDB related commands

---
+++ Command/GenerateDoctrineCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace IsmaAmbrosi\GeneratorBundle\Command;
+
+use Symfony\Bundle\DoctrineBundle\Mapping\MetadataFactory;
+use Symfony\Bundle\DoctrineBundle\Command\DoctrineCommand;
+
+abstract class GenerateDoctrineCommand extends DoctrineCommand
+{
+
+    protected function parseShortcutNotation($shortcut)
+    {
+        $document = str_replace('/', '\\', $shortcut);
+
+        if (false === $pos = strpos($document, ':')) {
+            throw new \InvalidArgumentException(sprintf('The document name must contain a : ("%s" given, expecting something like AcmeBlogBundle:Blog/Post)', $document));
+        }
+
+        return array(substr($document, 0, $pos), substr($document, $pos + 1));
+    }
+}
