625f8a183e779349b5e9742af276d4d2f6dbb6e6
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-12-06 01:34:09 -0200

    Improved simple generator test

--- Tests/Generator/DoctrineDocumentGeneratorTest.php
+++ Tests/Generator/DoctrineDocumentGeneratorTest.php
@@ -3,10 +3,11 @@
 namespace IsmaAmbrosi\Bundle\GeneratorBundle\Tests\Generator;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Finder\Finder;
 use IsmaAmbrosi\Bundle\GeneratorBundle\Generator\DoctrineDocumentGenerator;
 
 /**
- * Class
+ * Class DoctrineDocumentGeneratorTest
  *
  * @author Ismael Ambrosi<ismael@servergrove.com>
  */
@@ -19,18 +20,49 @@ class DoctrineDocumentGeneratorTest extends WebTestCase
         static::$kernel->boot();
     }
 
+    protected function tearDown()
+    {
+        $this->getFilesystem()->remove($this->getBundle()->getPath() . DIRECTORY_SEPARATOR . 'Document');
+    }
+
     public function testGenerator()
     {
-        $container = static::$kernel->getContainer();
-        $generator = new DoctrineDocumentGenerator($container->get('filesystem'), $container->get('doctrine.odm.mongodb.document_manager'));
+        $generator = new DoctrineDocumentGenerator($this->getFilesystem(), $this->getDocumentManager());
 
         $generator->generate(
-            static::$kernel->getBundle('IsmaAmbrosiGeneratorBundle'),
+            $bundle = $this->getBundle(),
             'Translation',
             array(
                  array('fieldName'=> 'locale',
                        'type'     => 'string')
             ), false
         );
+
+        $this->assertFileExists($bundle->getPath() . DIRECTORY_SEPARATOR . 'Document' . DIRECTORY_SEPARATOR . 'Translation.php');
+        $this->assertFileNotExists($bundle->getPath() . DIRECTORY_SEPARATOR . 'Document' . DIRECTORY_SEPARATOR . 'TranslationRepository.php');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpKernel\Bundle\BundleInterface
+     */
+    private function getBundle()
+    {
+        return static::$kernel->getBundle('IsmaAmbrosiGeneratorBundle');
+    }
+
+    /**
+     * @return \Doctrine\ODM\MongoDB\DocumentManager
+     */
+    private function getDocumentManager()
+    {
+        return static::$kernel->getContainer()->get('doctrine.odm.mongodb.document_manager');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpKernel\Util\Filesystem
+     */
+    private function getFilesystem()
+    {
+        return static::$kernel->getContainer()->get('filesystem');
     }
 }
