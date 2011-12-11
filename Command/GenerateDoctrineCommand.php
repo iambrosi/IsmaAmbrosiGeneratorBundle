217a0687a0e3b8e65b03f77447fa98b2e9861400
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-12-11 00:53:19 -0200

    Added common functions

--- Command/GenerateDoctrineCommand.php
+++ Command/GenerateDoctrineCommand.php
@@ -18,4 +18,48 @@ abstract class GenerateDoctrineCommand extends DoctrineCommand
 
         return array(substr($document, 0, $pos), substr($document, $pos + 1));
     }
+
+    /**
+     * @param $name
+     *
+     * @return \Symfony\Component\HttpKernel\Bundle\BundleInterface
+     */
+    protected function getBundle($name)
+    {
+        return $this->getContainer()->get('kernel')->getBundle($name);
+    }
+
+    /**
+     * @return \Doctrine\ODM\MongoDB\DocumentManager
+     */
+    protected function getDocumentManager()
+    {
+        return $this->getContainer()->get('doctrine.odm.mongodb.document_manager');
+    }
+
+    /**
+     * @param string $bundle
+     *
+     * @return string
+     */
+    protected function getDocumentNamespace($bundle)
+    {
+        return $this->getBundle($bundle)->getNamespace().'\\Document';
+    }
+
+    /**
+     * @param $documentClass
+     *
+     * @return \Doctrine\ODM\MongoDB\Mapping\ClassMetadata
+     */
+    protected function getDocumentMetadata($documentClass)
+    {
+        $factory = new \Doctrine\ODM\MongoDB\Mapping\ClassMetadataFactory();
+        $factory->setDocumentManager($this->getDocumentManager());
+        $factory->setConfiguration($this->getDocumentManager()->getConfiguration());
+
+        $metadata = $factory->getMetadataFor($documentClass);
+
+        return $metadata;
+    }
 }
