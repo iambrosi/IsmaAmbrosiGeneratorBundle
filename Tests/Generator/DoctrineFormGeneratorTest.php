9eb717b5b933b5ddc77c5da0933479976e2d6d3f
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-12-12 10:29:10 -0200

    New test for DoctrineFormGenerator

---
+++ Tests/Generator/DoctrineFormGeneratorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace IsmaAmbrosi\Bundle\GeneratorBundle\Tests\Generator;
+
+use IsmaAmbrosi\Bundle\GeneratorBundle\Tests\WebTestCase;
+use IsmaAmbrosi\Bundle\GeneratorBundle\Generator\DoctrineFormGenerator;
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadataInfo;
+
+/**
+ * Class DoctrineDocumentGeneratorTest
+ *
+ * @author Ismael Ambrosi<ismael@servergrove.com>
+ */
+class DoctrineFormGeneratorTest extends GeneratorTest
+{
+
+    public function testSimpleGenerator()
+    {
+        $generator = new DoctrineFormGenerator($this->getFilesystem(), $this->getBundle()->getPath().'/Resources/skeleton/form');
+        $generator->generate($this->getTestBundle(), $this->documentName, $this->metadata);
+
+        $this->assertFileExists($file = $this->getTestBundle()->getPath().'/Form/'.$this->documentName.'Type.php');
+
+        $content = file_get_contents($file);
+        $this->assertContains('->add(\'name\')', $content);
+        $this->assertContains('->add(\'description\')', $content);
+        $this->assertContains('class '.$this->documentName.'Type extends AbstractType', $content);
+    }
+}
