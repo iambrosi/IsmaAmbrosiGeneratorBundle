c089970ebc601791c39f60bd7ffb28e18512e3d7
Ismael Ambrosi <ismaambrosi@gmail.com>
2012-06-12 10:56:10 -0300

    Updated buildForm function signature

--- Resources/skeleton/form/FormType.php
+++ Resources/skeleton/form/FormType.php
@@ -3,11 +3,11 @@
 namespace {{ namespace }}\Form{{ document_namespace ? '\\' ~ document_namespace : '' }};
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class {{ form_class }} extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
         {%- for field in fields %}
